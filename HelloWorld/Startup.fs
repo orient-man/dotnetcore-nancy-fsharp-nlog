299db1481fde23da4ad43eaefab2e3534ce6bb05
Marcin Malinowski <marcin.malinowski@finai.com>
2017-04-10 14:22:21 +0200

    ElasticSearch URL from appsettings.json

--- HelloWorld/Startup.fs
+++ HelloWorld/Startup.fs
@@ -1,9 +1,23 @@
 ﻿namespace MvcApp
 
+open System.IO
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Owin
+open Microsoft.Extensions.Configuration
 open Nancy.Owin
 
 type Startup () =
+    let builder =
+        ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true)
+
+    let configuration = builder.Build()
+
+    do
+        "ElasticSearch"
+        |> configuration.GetConnectionString
+        |> printfn "ElasticSearch: %s"
+
     member __.Configure(app: IApplicationBuilder) =
         app.UseOwin(fun buildFunc -> buildFunc.UseNancy() |> ignore) |> ignore
