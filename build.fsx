e7be1d22baf2160b4e1e6178442504e5e64f623c
Marcin Malinowski <marcin.malinowski@finai.com>
2017-04-13 15:14:51 +0200

    FAKE script should do everything that VSTS does

--- build.fsx
+++ build.fsx
@@ -2,16 +2,27 @@
 #r "packages/FAKE/tools/FakeLib.dll"
 open Fake
 
+let configuration = getBuildParamOrDefault "Configuration" "Debug"
+let output = getBuildParamOrDefault "Output" ""
+
 Target "Restore" <| fun _ -> DotNetCli.Restore id
 
-Target "Build" <| fun _ -> DotNetCli.Build id
+Target "Build" <| fun _ ->
+    DotNetCli.Build <| fun p -> { p with Configuration = configuration }
 
 Target "Test" <| fun _ ->
     !! "src/**/*Tests.fsproj"
     |> Seq.iter (fun proj ->
-        DotNetCli.Test <| fun p -> { p with Project = proj })
+        DotNetCli.Test <| fun p ->
+            { p with
+                Configuration = configuration
+                Project = proj })
 
-Target "Publish" <| fun _ -> DotNetCli.Publish id
+Target "Publish" <| fun _ ->
+    DotNetCli.Publish <| fun p ->
+        { p with
+            Configuration = configuration
+            Output = output }
 
 "Restore"
     ==> "Build"
