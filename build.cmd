cf0c9488a13e7fd3de8b718ee490044c6639f4ce
Marcin Malinowski <marcin.malinowski@finai.com>
2017-04-13 16:35:09 +0200

    .paket\paket.bootstrapper.exe

--- build.cmd
+++ build.cmd
@@ -1,5 +1,14 @@
 @echo off
 cls
 
-cd %~dp0
+.paket\paket.bootstrapper.exe
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+rem .paket\paket.exe restore
+rem if errorlevel 1 (
+rem  exit /b %errorlevel%
+rem )
+
 packages\FAKE\tools\FAKE.exe build.fsx %*
